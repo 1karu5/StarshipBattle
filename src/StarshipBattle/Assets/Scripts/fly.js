7ab4ab348ab3c095f6e89f6ea2087dbfcba1d2a3
1karu5 <1karu5@users.noreply.github.com>
2013-12-04 11:58:14 +0100

    tests for shooting with lasers

---
+++ src/StarshipBattle/Assets/Scripts/fly.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+	rigidbody.AddForce(Vector3.left*3000);
+}
+
+function OnCollisionEnter (c : Collision) {
+	Debug.Log("hit");
+}
---
+++ src/StarshipBattle/Assets/Scripts/shoot.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var prefab : GameObject;
+public var freq : int;
+
+private var timer : float = 0;
+
+function Update () {
+ 
+ 	timer += Time.deltaTime;
+ 	//Debug.Log(timer);
+	if(timer > freq){
+		timer =0;
+		Instantiate (prefab, transform.position, Quaternion.identity);
+	}
+}
