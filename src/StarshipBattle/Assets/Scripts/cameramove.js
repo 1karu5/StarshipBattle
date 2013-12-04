07b2cbd7add6c2fa5be9b42449ee32b924963ca4
Pavel Andrejev <andrejev@users.noreply.github.com>
2013-12-04 10:50:42 +0100

    camera move zoom factor etc.

--- src/StarshipBattle/Assets/Scripts/cameramove.js
+++ src/StarshipBattle/Assets/Scripts/cameramove.js
@@ -3,38 +3,57 @@
 var scrollArea:int = 20;
 var scrollSpeed:int = 100;
 var zoomSpeed:int = 300;
+var maxZoomFactor:float = 2;
+
+private var xPos:float; 
+private var yPos:float; 
+private var zPos:float;
+private var fieldOfView:float;
 
 function Start(){
-
+	xPos = transform.position.x;
+	yPos = transform.position.y;
+	zPos = transform.position.z;
+	fieldOfView = camera.fieldOfView;
+	Debug.Log(fieldOfView);
 }
 
 function Update(){
     var mPosX = Input.mousePosition.x;
     var mPosY = Input.mousePosition.y;
     
+    
+    
     // Do camera movement by mouse position
-    if (mPosX < scrollArea) {
-        transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);
-    }
-    if (mPosX >= Screen.width - scrollArea) {
-        transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
-    }
-    if (mPosY < scrollArea) {
-        transform.Translate(Vector3.up * -scrollSpeed * Time.deltaTime);
-    }
-    if (mPosY >= Screen.height - scrollArea) {
-        transform.Translate(Vector3.up * scrollSpeed * Time.deltaTime);
+    if (transform.position.y < yPos){
+    	var zoomPx:float = yPos - transform.position.y;
+    	var movePx:float = Mathf.Min(zoomPx, scrollSpeed * Time.deltaTime);
+    	
+	    if (mPosX < scrollArea && (xPos - transform.position.x) < zoomPx) {
+	        transform.Translate(Vector3.left * movePx);
+	    }
+	    if (mPosX >= Screen.width - scrollArea && (transform.position.x - xPos) < zoomPx) {
+	        transform.Translate(Vector3.right * movePx);
+	    }
+	    if (mPosY < scrollArea && (zPos - transform.position.z) < zoomPx) {
+	        transform.Translate(Vector3.down * movePx);
+	    }
+	    if (mPosY >= Screen.height - scrollArea && (transform.position.z - zPos) < zoomPx) {
+	        transform.Translate(Vector3.up * movePx);
+	    }
     }
     
     
-    if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+    if (Input.GetAxis("Mouse ScrollWheel") < 0  && transform.position.y < yPos) // back
     {
+    		
         transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
         //Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize-1, 1);
     }
-    if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+    if (Input.GetAxis("Mouse ScrollWheel") > 0 && transform.position.y > (yPos / 2)) // forward
     {
         transform.Translate(Vector3.back * -zoomSpeed * Time.deltaTime);
+        
         //Camera.main.orthographicSize = Mathf.Min(Camera.main.orthographicSize-1, 6);
     }
     
