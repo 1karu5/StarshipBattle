8d7c597653779c4a2ea1605f69984c955d15454c
Philipp Sander <philippsander88@gmx.net>
2014-01-06 21:09:05 +0100

    added key controller

---
+++ src/StarshipBattle/Assets/Scripts/keyController.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+enum SBStep { choseChar, choseRoom, choseTarget};
+enum SBChar { gun1, gun2, eng};
+enum SBRoom { cannon1, cannon2, medic, UnityEngine};
+
+private var state_r:SBStep;
+private var char_r:SBChar;
+private var room_r:SBRoom;
+private var target_r:SBRoom;
+
+private var state_l:SBStep;
+private var char_l:SBChar;
+private var room_l:SBRoom;
+private var target_l:SBRoom;
+
+function Start () {
+
+	state_r = SBStep.choseChar;
+	char_r = null;
+	room_r = null;
+	target_r = null; 
+	
+	state_l = SBStep.choseChar;
+	char_l = null;
+	room_l = null;
+	target_l = null; 
+	
+}
+
+function Update () {
+
+	//TODO check in what step the player is and what he can chose
+	
+}
