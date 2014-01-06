290267ff1767e1e1a752071280b06aa9fff09e00
Pavel Andrejev <andrejev@users.noreply.github.com>
2014-01-06 19:57:51 +0100

    sync

--- src/StarshipBattle/Assets/Scripts/cannonController.js
+++ src/StarshipBattle/Assets/Scripts/cannonController.js
@@ -10,8 +10,7 @@ private static var rotateToRaum:Array = [0,15,30,45];
 //			Back
 // Raum 0-3
 // 	 	front to back
-public static function rotateTo(player:String, cannon:String, raum:int){
-	
+public static function rotateTo(player:String, cannon:String, raum:int){	
 	var cannonObject = GameObject.Find(player).transform.FindChild("Cannon" + cannon).gameObject;
 	var bbb:int = (cannon == "Front"? 1: -1);
 	var aaa:int = rotateToRaum[raum];
