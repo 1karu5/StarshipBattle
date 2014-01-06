5f22fa18f18ffbcfd1122f71d014781359193baa
Pavel Andrejev <andrejev@users.noreply.github.com>
2014-01-06 20:07:20 +0100

    player1Keys fixed

--- src/StarshipBattle/Assets/Scripts/player1Keys.js
+++ src/StarshipBattle/Assets/Scripts/player1Keys.js
@@ -1,21 +1,13 @@
 ﻿#pragma strict
 
-public static var prevObject:KeyCode = KeyCode.Q;
-public static var nextObject:KeyCode = KeyCode.W;
+public class Player1Keys {
 
-public static var prevStep:KeyCode = KeyCode.Y;
-public static var nextStep:KeyCode = KeyCode.X;
+	public static var prevObject:KeyCode = KeyCode.Q;
+	public static var nextObject:KeyCode = KeyCode.W;
 
-public static var shield:KeyCode = KeyCode.C;
+	public static var prevStep:KeyCode = KeyCode.Y;
+	public static var nextStep:KeyCode = KeyCode.X;
 
+	public static var shield:KeyCode = KeyCode.C;
 
-
-function Start () {
-
-}
-
-function Update () {
-	if (Input.GetKeyDown(shield)){
-		schildController.sichtbar("playerLeft");
-	}
 }
