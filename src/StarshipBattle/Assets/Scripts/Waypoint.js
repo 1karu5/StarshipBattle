6d3149c2e7c2a335d99029cc499b9fab44af2687
Philipp Sander <philippsander88@gmx.net>
2014-01-10 10:36:42 +0100

    added waypoints for left player

---
+++ src/StarshipBattle/Assets/Scripts/Waypoint.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var neighbors:GameObject[];
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnDrawGizmos()
+{
+	Gizmos.color = Color (1,0,0,.5);
+	Gizmos.DrawCube (transform.position, Vector3 (0.2,0.2,0.2));
+	
+	for (var n:GameObject in neighbors)
+	{
+		Gizmos.DrawLine(this.transform.position, n.transform.position);
+	}
+}
