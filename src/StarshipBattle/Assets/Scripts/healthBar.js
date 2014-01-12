777171d607341c538ce00bc4e7c3bd4c3a911661
1karu5 <1karu5@users.noreply.github.com>
2014-01-12 23:10:50 +0100

    healthbar

---
+++ src/StarshipBattle/Assets/Scripts/healthBar.js
@@ -0,0 +1,20 @@
+﻿public var ownerName : String;
+public var character:String;
+public var healthTex:Texture;
+
+private var health : float;
+
+private var healthBarHeight : int= 5;
+private var healthBarLeft : int= 50;
+private var barTop : int= 1;
+private var healthBarLength : float= 100;
+
+function Update () {
+	healthBarLength = (healthController.getHealth(ownerName, character) /250) * 100;
+}
+
+function OnGUI () {
+	var screenPosition = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y,transform.position.z));
+	GUI.Box(new Rect(screenPosition.x - healthBarLeft / 2, Screen.height - screenPosition.y - barTop,healthBarLength, healthBarHeight), "");
+	GUI.DrawTexture(new Rect(screenPosition.x - healthBarLeft / 2,Screen.height - screenPosition.y - barTop,healthBarLength, healthBarHeight), healthTex);	
+}	
